d4e3f7c8f9e45ff3f7aea4331844c6d46c36880e
Samuel Vange <samuelvange@gmail.com>
2018-09-22 22:51:22 -0700

    updated Sam's scaffolding for Bonetrousle

--- HackerRank/Bonetrousle/Sam/Bonetrousle.py
+++ HackerRank/Bonetrousle/Sam/Bonetrousle.py
@@ -13,15 +13,15 @@ def bonetrousle(n, k, b):
 
 class TestBonetrousle(unittest.TestCase):
     def test_bonetrousle_naive(self):
-        actual = [[1, 3, 8], [1, 4, 7], [1, 5, 6], [2, 3, 7], [2, 4, 6], [3, 4, 5]]
-        solution = list(bonetrousle_naive(12, 8, 3))
-        actual.sort()
-        solution.sort()
-        self.assertEqual(solution, actual)
+        actual = [[1, 3, 8], [1, 4, 7], [2, 3, 7], [1, 5, 6], [2, 4, 6], [3, 4, 5]]
+        solutions = list(bonetrousle_naive(12, 8, 3)) or [[-1]]
+
+        self.assertEqual(solutions, actual)
         self.assertEqual(list(bonetrousle_naive(10, 3, 3)), [])
 
     def test_bonetrousle(self):
-        self.assertIn(bonetrousle(12, 8, 3), list(bonetrousle_naive(12, 8, 3)))
+        self.assertIn(bonetrousle(12, 8, 3), list(bonetrousle_naive(12, 8, 3)) or [[-1]])
+        self.assertIn(bonetrousle(10, 3, 3), list(bonetrousle_naive(10, 3, 3)) or [[-1]])
 
 
 if __name__ == '__main__':
