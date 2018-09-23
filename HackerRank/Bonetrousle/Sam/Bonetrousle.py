025ca759fa387c923d235246baf34c254166b5e5
Samuel Vange <samuelvange@gmail.com>
2018-09-23 07:32:12 -0700

    created fast solution, needs cleanup, too many slices while distributing remainder

--- HackerRank/Bonetrousle/Sam/Bonetrousle.py
+++ HackerRank/Bonetrousle/Sam/Bonetrousle.py
@@ -8,11 +8,27 @@ def bonetrousle_naive(n, k, b):
 
 
 def bonetrousle(n, k, b):
-    pass
+    # A solution is only possible if n is between the smallest and largest possible solutions
+    max_possible = sum(range(k - (b - 1), k + 1))
+    min_possible = sum(range(1, b + 1))
+    if not min_possible <= n <= max_possible:
+        return [-1]
+
+    # Start with the smallest possible solution
+    solution = list(range(1, b + 1))
+
+    # We'll divide our target number to distribute over our solution
+    min_to_add = (n - min_possible) // b
+    solution = list(map(lambda x: x + min_to_add, solution))
+
+    # Distribute remainder
+    remainder = (n - min_possible) % b
+    solution = [num + 1 if index < remainder else num for index, num in enumerate(solution[::-1])][::-1] # too many slices, cleanup
+    return solution
 
 
 class TestBonetrousle(unittest.TestCase):
-    def test_bonetrousle_naive(self):
+    def skip_test_bonetrousle_naive(self):
         actual = [[1, 3, 8], [1, 4, 7], [2, 3, 7], [1, 5, 6], [2, 4, 6], [3, 4, 5]]
         solutions = list(bonetrousle_naive(12, 8, 3)) or [[-1]]
 
