d85ea0fabdc351c7b47081f3bf02d0f6ccb33f9a
Samuel Vange <samuelvange@gmail.com>
2018-10-04 20:26:15 -0700

    went too far with the functions

--- HackerRank/3DSurfaceArea/Sam/ThreeDSurfaceArea.py
+++ HackerRank/3DSurfaceArea/Sam/ThreeDSurfaceArea.py
@@ -4,44 +4,61 @@ from collections import namedtuple
 # bad naming convention on hackerrank
 def surfaceArea(A):
     running_total = 0
-    max_x = len(A) - 1
-    max_y = len(A[0]) - 1
     Square = namedtuple('Square', ['x', 'y'])
 
     for x, row in enumerate(A):
         for y, square_height in enumerate(row):
             # add 1 unit each for top and bottom
             running_total += 2
+            this_square = Square(x, y)
 
-            # find surrounding squares
-            square_to_the_left = Square(x, y - 1)
-            square_to_the_right = Square(x, y + 1)
-            square_to_the_up = Square(x + 1, y)
-            square_to_the_down = Square(x - 1, y)
+            # find surrounding squares with
+            # FUNCTION OVERKILL!!!!
+            def north_adjacent(square):
+                max_x = len(A) - 1
+                if square.x + 1> max_x:
+                    return None
+                else:
+                    return Square(square.x + 1, square.y)
 
-            if square_to_the_left.y < 0:
-                running_total += square_height
-            else:
-                if square_height - A[square_to_the_left.x][square_to_the_left.y] > 0:
-                    running_total += square_height - A[square_to_the_left.x][square_to_the_left.y]
+            def east_adjacent(square):
+                max_y = len(A[0]) - 1
+                if square.y + 1 > max_y:
+                    return None
+                else:
+                    return Square(square.x, square.y + 1)
 
-            if square_to_the_right.y > max_y:
-                running_total += square_height
-            else:
-                if square_height - A[square_to_the_right.x][square_to_the_right.y] > 0:
-                    running_total += square_height - A[square_to_the_right.x][square_to_the_right.y]
+            def south_adjacent(square):
+                if square.x - 1 < 0:
+                    return None
+                else:
+                    return Square(square.x - 1, square.y)
 
-            if square_to_the_up.x > max_x:
-                running_total += square_height
-            else:
-                if square_height - A[square_to_the_up.x][square_to_the_up.y] > 0:
-                    running_total += square_height - A[square_to_the_up.x][square_to_the_up.y]
+            def west_adjacent(square):
+                if square.y - 1 < 0:
+                    return None
+                else:
+                    return Square(square.x, square.y - 1)
 
-            if square_to_the_down.x < 0:
-                running_total += square_height
-            else:
-                if square_height - A[square_to_the_down.x][square_to_the_down.y] > 0:
-                    running_total += square_height - A[square_to_the_down.x][square_to_the_down.y]
+            def num_visible_squares(square, adjacent_square):
+                if square_height(square) - square_height(adjacent_square) > 0:
+                    return square_height(square) - square_height(adjacent_square)
+                else:
+                    return 0
+
+            def square_height(square):
+                if square:
+                    return A[square.x][square.y]
+                else:
+                    return 0
+
+            running_total += num_visible_squares(this_square, west_adjacent(this_square))
+
+            running_total += num_visible_squares(this_square, east_adjacent(this_square))
+
+            running_total += num_visible_squares(this_square, north_adjacent(this_square))
+
+            running_total += num_visible_squares(this_square, south_adjacent(this_square))
 
     return running_total
 
